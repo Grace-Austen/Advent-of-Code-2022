4bf00b92310c84604baa8e3a35fba6b4594f1610
Grace-Austen <austengracel@gmail.com>
2022-12-11 22:15:24 -0500

    Day 1 part 2 complete, ruby is pretty cool though

---
+++ Day 1/part2.rb
@@ -0,0 +1,18 @@
+
+inFile = File::open("input_1.txt")
+
+value = 0
+elves = Array.new()
+
+loop do
+    nextLine = inFile.readline
+    if nextLine != "\n"
+        value += nextLine.to_i
+    else
+        elves.push(value)
+        value = 0
+    end
+break if inFile.eof?
+end
+
+puts elves.max(3).sum
