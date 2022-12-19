9b2d42efb8ad67c1a2bea0930bf05a7b7773602e
Grace-Austen <austengracel@gmail.com>
2022-12-19 09:45:44 -0500

    d5 part2
    Reversed the order of the cargo stacks for this one (there's a lot of
    context I'm not going to explain here)
    Shift is cool and I appreciate that adding to the beginning of the array
    can be referred to as "unshift". I'm sure there was a faster way to add
    the individual elements of the shifted stack than to unshift and flatten
    but it would've required more lines of code and the stacks are bounded
    in size.

--- Day 5/part2.rb
+++ Day 5/part2.rb
@@ -6,8 +6,8 @@ cargo = Array.new(9) {Array.new}
 
 puts cargo.to_s
 
-# 0 is bottom of cargo stack
-7.downto(0){ |line|
+# 0 is top of cargo stack
+0.upto(7){ |line|
     puts lines[line]
     start = 1
     for i in 0..8
@@ -21,10 +21,8 @@ instructions = lines[10..-1]
 
 for line in instructions
     sep = line.split(" ")
-    sep[1].to_i.times{
-        crate = cargo[sep[3].to_i-1].pop
-        cargo[sep[5].to_i-1].push(crate)
-    }
+    stack = cargo[sep[3].to_i-1].shift(sep[1].to_i)
+    cargo[sep[5].to_i-1].unshift(stack).flatten!(1)
 end
 
 puts cargo.to_s
@@ -32,7 +30,7 @@ puts cargo.to_s
 answer = []
 
 for stack in cargo
-    answer<<stack[-1]
+    answer<<stack[0]
 end
 
 puts answer.join
